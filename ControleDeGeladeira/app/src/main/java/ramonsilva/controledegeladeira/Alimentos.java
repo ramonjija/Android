959e4cad51ec01c481b6b40a302c91d33336b6b5
Ramon Silva <ramon.silva@al.infnet.edu.br>
2015-08-13 16:56:25 -0300

    Alteração no codigo para inserção de abas

---
+++ ControleDeGeladeira/app/src/main/java/ramonsilva/controledegeladeira/Alimentos.java
@@ -0,0 +1,30 @@
+package ramonsilva.controledegeladeira;
+
+/**
+ * Created by ramon.silva on 13/08/2015.
+ */
+public class Alimentos {
+    private String nome;
+    private String quantidade;
+
+    public String getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(String quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return nome+" | "+quantidade;
+    }
+}
--- ControleDeGeladeira/app/src/main/java/ramonsilva/controledegeladeira/MainActivity.java
+++ ControleDeGeladeira/app/src/main/java/ramonsilva/controledegeladeira/MainActivity.java
@@ -1,5 +1,6 @@
 package ramonsilva.controledegeladeira;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.app.ListActivity;
 import android.support.v7.app.ActionBarActivity;
@@ -8,6 +9,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TabHost;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -15,23 +17,24 @@ import java.util.ArrayList;
 
 public class MainActivity extends Activity {
 
-    ArrayList<String> listaDeAlimentos = new ArrayList<String>();
-    ArrayAdapter adapter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TabHost tab = (TabHost)findViewById(R.id.tabHost);
+        tab.setup();
 
-        listaDeAlimentos.add("Maca");
-        listaDeAlimentos.add("Limao");
-        listaDeAlimentos.add("Banana");
+        TabHost.TabSpec descritor = tab.newTabSpec("aba1");
+        descritor.setContent(R.id.idListViewAlimentos);
+        descritor.setIndicator(getString(R.string.titutoApp));
+        tab.addTab(descritor);
 
+        descritor = tab.newTabSpec("aba2");
+        descritor.setContent(R.id.Cadastro);
+        descritor.setIndicator(getString(R.string.cadastro));
+        tab.addTab(descritor);
 
-        ListView lista = (ListView)findViewById(R.id.idListView);
-             adapter =  new ArrayAdapter(this,
-                        android.R.layout.simple_list_item_2, listaDeAlimentos);
-        lista.setAdapter(adapter);
 
     }
 
