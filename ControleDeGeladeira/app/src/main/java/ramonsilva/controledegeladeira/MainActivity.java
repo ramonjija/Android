0846139b0d8a326a7e511466cb5763190021113c
Ramon Silva <ramon.silva@al.infnet.edu.br>
2015-08-14 11:05:52 -0300

    Inclusão da lista e do adaptador

--- ControleDeGeladeira/app/src/main/java/ramonsilva/controledegeladeira/MainActivity.java
+++ ControleDeGeladeira/app/src/main/java/ramonsilva/controledegeladeira/MainActivity.java
@@ -7,16 +7,40 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TabHost;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.List;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener{
 
+    private List<Alimentos> alimentos = new ArrayList<Alimentos>();
+    private  AlimentoAdapter adapter = null;
+
+    @Override
+    public void onClick(View v) {
+        Alimentos alimento = new Alimentos();
+        EditText nome = (EditText)findViewById(R.id.idNome);
+        EditText quantidade = (EditText)findViewById(R.id.idQuantidade);
+        alimento.setNome(nome.getText().toString());
+        alimento.setQuantidade(quantidade.getText().toString());
+        adapter.add(alimento);
+        nome.setText("");
+        quantidade.setText("");
+    }
+
+    private class AlimentoAdapter extends ArrayAdapter{
+            public AlimentoAdapter(){
+                super(MainActivity.this, android.R.layout.simple_list_item_1, alimentos);
+            }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +59,18 @@ public class MainActivity extends Activity {
         descritor.setIndicator(getString(R.string.cadastro));
         tab.addTab(descritor);
 
+        Button salvar = (Button) findViewById(R.id.idBtnSalvar);
+        salvar.setOnClickListener(this);
+
+        ListView lista = (ListView)findViewById(R.id.idListViewAlimentos);
+        adapter = new AlimentoAdapter();
+        lista.setAdapter(adapter);
+
 
     }
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
